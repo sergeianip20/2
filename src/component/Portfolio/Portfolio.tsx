30bbbd7534e8b8bf30199dff95c144cc41ce5d0d
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-07-08 17:35:12 +0300

    Update Portfolio.tsx

--- src/component/Portfolio/Portfolio.tsx
+++ src/component/Portfolio/Portfolio.tsx
@@ -1,71 +1,58 @@
 import s from './Portfolio.module.css'
-import Web1 from '../../imgs/img/web-1.jpg'
-import Web2 from '../../imgs/img/web-2.jpg'
-import Web3 from '../../imgs/img/web-3.jpg'
-import Web4 from '../../imgs/img/web-4.jpg'
+
 export const MyPortfolio=()=> {
     return <section className="section bg-custom-gray" id="portfolio">
          <div className="container">
-                <h1 className="mb-5"><span className="text-danger">My</span> Portfolio</h1>
+                <h1 className="mb-5"><span className="text-danger">Мое</span> Портфолио</h1>
 
                 <div className="portfolio">
                     <div className="portfolio-container">
 <div className={s.flex}>
                         <div className="col-md-6 col-lg-4 web new">
                             <div className="portfolio-item">
-                                <img src={Web1} className="img-fluid"
-                                     alt="Download free bootstrap 4 admin dashboard, free boootstrap 4 templates"/>
+                                
+                                <iframe  className={s.iframe}  src={"https://final-social.vercel.app/"}></iframe>
                                     <div className="content-holder">
-                                        <a className="img-popup" href="assets/imgs/web-1.jpg"></a>
+                                        <a className="img-popup" href="https://final-social.vercel.app/">
                                         <div className="text-holder">
                                             <h6 className="title">WEB</h6>
                                             <p className="subtitle">Expedita corporis doloremque velit in totam!</p>
                                         </div>
+                                        </a>
                                     </div>
                             </div>
                         </div>
                         <div className="col-md-6 col-lg-4 web new">
                             <div className="portfolio-item">
-                                <img src={Web2} className="img-fluid"
-                                     alt="Download free bootstrap 4 admin dashboard, free boootstrap 4 templates"/>
+                                <iframe className={s.iframe}  style={{border:'none'}}  src={"https://final-card.vercel.app/#/auth/login"}></iframe>
+
                                     <div className="content-holder">
-                                        <a className="img-popup" href="assets/imgs/web-2.jpg"></a>
+                                        <a className="img-popup" href="https://final-card.vercel.app/#/auth/login">
                                         <div className="text-holder">
                                             <h6 className="title">WEB</h6>
                                             <p className="subtitle">Expedita corporis doloremque velit in totam!</p>
                                         </div>
+                                        </a>
                                     </div>
                             </div>
                         </div>
 
                         <div className="col-md-6 col-lg-4 web">
                             <div className="portfolio-item">
-                                <img src={Web4} className="img-fluid"
-                                     alt="Download free bootstrap 4 admin dashboard, free boootstrap 4 templates"/>
+
+                                <iframe className={s.iframe}   src={"https://finak-todolist.vercel.app/login"}> </iframe>
                                     <div className="content-holder">
-                                        <a className="img-popup" href="assets/imgs/web-4.jpg"></a>
+                                        <a className="img-popup" href="https://finak-todolist.vercel.app/login">
                                         <div className="text-holder">
                                             <h6 className="title">WEB</h6>
                                             <p className="subtitle">Expedita corporis doloremque velit in totam!</p>
                                         </div>
+                                        </a>
                                     </div>
                             </div>
                         </div>
 
 
-                        <div className="col-md-6 col-lg-4 web new">
-                            <div className="portfolio-item">
-                                <img src={Web3} className="img-fluid"
-                                     alt="Download free bootstrap 4 admin dashboard, free boootstrap 4 templates"/>
-                                    <div className="content-holder">
-                                        <a className="img-popup" href="assets/imgs/web-3.jpg"></a>
-                                        <div className="text-holder">
-                                            <h6 className="title">WEB</h6>
-                                            <p className="subtitle">Expedita corporis doloremque velit in totam!</p>
-                                        </div>
-                                    </div>
-                            </div>
-                        </div>
 
                     </div>
                     </div>
