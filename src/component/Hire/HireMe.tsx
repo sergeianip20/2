d8c50835012e33c58744381216c6f98ff4cc4151
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-07-08 17:34:32 +0300

    Update HireMe.tsx

--- src/component/Hire/HireMe.tsx
+++ src/component/Hire/HireMe.tsx
@@ -1,12 +1,10 @@
-
-
 export const HireMe=()=> {
     return <div>
         <section className="section bg-dark py-5">
             <div className="container text-center">
-                <h2 className="text-light mb-5 font-weight-normal">looking at options
+                <h2 className="text-light mb-5 font-weight-normal">Рассматривают варианты
                 </h2>
-                <button className="btn bg-primary w-lg">Hire me</button>
+                <button className="btn bg-primary w-lg"><a className={'header_link'} href={'https://hh.ru/resume/a4a84f1dff09a113d40039ed1f704572574454'}>Нанять Меня</a></button>
             </div>
         </section>
 
