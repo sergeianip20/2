783c16cb5787c4fb151de74d359d90a9054f6974
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-07-08 17:34:11 +0300

    Update Header.tsx

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -22,10 +22,10 @@ export const Header=()=> {
                     </li>
                 </ul>
                 <div className="header-content">
-                    <h4 className="header-subtitle">Hello, I am</h4>
-                    <h1 className="header-title">Sergei Anipchenko</h1>
+                    <h4 className="header-subtitle">Привет, я </h4>
+                    <h1 className="header-title">Анипченко Сергей</h1>
                     <h6 className="header-mono">Frontend  Developer</h6>
-                    <button className="btn btn-primary btn-rounded"><i className="ti-printer pr-2"></i>Print Resume
+                    <button className="btn btn-primary btn-rounded"><i className="ti-printer pr-2"></i> <a className={'header_link'} href={'https://hh.ru/resume/a4a84f1dff09a113d40039ed1f704572574454'}> Resume </a>
                     </button>
                 </div>
             </div>
