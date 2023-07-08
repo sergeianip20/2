ed39aa285acfe324f2cd86c18e77ec4e0c2fa230
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-07-08 17:36:17 +0300

    Update ContainerFliud.tsx

--- src/component/ContainerFliud.tsx
+++ src/component/ContainerFliud.tsx
@@ -5,16 +5,18 @@ export const ContainerFliud=()=> {
         <div className="container-fluid">
             <div id="about" className="row about-section">
                 <div className="col-lg-4 about-card">
-                    <h3 className="font-weight-light">Who am I ?</h3>
+                    <h3 className="font-weight-light">Обо мне</h3>
                     <span className="line mb-5"></span>
-                    <h5 className="mb-3">A Web  Developer Located In Our Lovely Earth</h5>
-                    <p className="mt-20">Lorem ipsum dolor sit amet, consectetur adipisicing elit.sit amet, Qui deserunt
-                        consequatur fugit repellendusillo voluptas?</p>
-                    <button className="btn btn-outline-danger"><i className="icon-down-circled2 "></i>Download My CV
-                    </button>
+                    <div>Я front-end разработчик с опытом создания SPA
+                        используя React, Redux, TypeScript, JavaScript, CSS, HTML.
+                        Постоянно совершенствую свои навыки, изучая новые технологии.
+                        В целях изучить Angular и React Native в будущем.
+                        Мои сильные стороны: аналитические способности, ответственность и стрессоустойчивость.
+                        Готов рассмотреть проектную работу и полную занятость.</div>
+
                 </div>
                 <div className="col-lg-4 about-card">
-                    <h3 className="font-weight-light">Personal Info</h3>
+                    <h3 className="font-weight-light">Персональная Информация</h3>
                     <span className="line mb-5"></span>
                     <ul className="mt40 info list-unstyled">
                         <li><span>Birthdate</span> : 21/07/1997</li>
@@ -42,14 +44,14 @@ export const ContainerFliud=()=> {
                     </ul>
                 </div>
                 <div className="col-lg-4 about-card">
-                    <h3 className="font-weight-light">My Expertise</h3>
+                    <h3 className="font-weight-light">Моя экспертиза</h3>
                     <span className="line mb-5"></span>
 
                     <div className="row">
                         <div className="col-1 text-danger pt-1"><i className="ti-paint-bucket icon-lg"></i></div>
                         <div className="col-10 ml-auto mr-3">
                             <h6>Web Development</h6>
-                            <p className="subtitle">Lorem ipsum dolor sit consectetur.</p>
+
                             <hr/>
                         </div>
                     </div>
