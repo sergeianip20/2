0acdbaa7f0844a58826b4380b721691b79a0ec30
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-07-10 20:55:04 +0300

    Update Nav.tsx

--- src/component/Nav/Nav.tsx
+++ src/component/Nav/Nav.tsx
@@ -1,44 +1,82 @@
 import avatar from '../../imgs/img/avatar.jpg'
-
+import React , {useState} from 'react'
 export const Nav =()=> {
+const [open, setOpen] = useState(true)
     return <div>
 
         <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-white" data-spy="affix"
              data-offset-top="510">
-            <div className="container">
-                <button className="navbar-toggler ml-auto" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse mt-sm-20 navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <a href="#home" className="nav-link">Главная</a>
-                        </li>
+            {open ?
+                <div className="container">
+                    <button className="navbar-toggler ml-auto" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" onClick={()=>{setOpen(false)}}></span>
+                    </button>
+                    <div className="collapse mt-sm-20 navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <a href="#home" className="nav-link">Главная</a>
+                            </li>
 
-                        <li className="nav-item">
-                            <a href="#resume" className="nav-link">Резюме</a>
-                        </li>
-                    </ul>
-                    <ul className="navbar-nav brand">
-                        <img src={avatar} alt="" className="brand-img"/>
+                            <li className="nav-item">
+                                <a href="#resume" className="nav-link">Резюме</a>
+                            </li>
+                        </ul>
+                        <ul className="navbar-nav brand">
+                            <img src={avatar} alt="" className="brand-img"/>
                             <li className="brand-txt">
                                 <h5 className="brand-title"></h5>
                                 <div className="brand-subtitle"> Developer</div>
                             </li>
-                    </ul>
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <a href="#portfolio" className="nav-link">Портфолио</a>
-                        </li>
+                        </ul>
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <a href="#portfolio" className="nav-link">Портфолио</a>
+                            </li>
 
-                        <li className="nav-item last-item">
-                            <a href="#contact" className="nav-link">Контакты</a>
-                        </li>
-                    </ul>
+                            <li className="nav-item last-item">
+                                <a href="#contact" className="nav-link">Контакты</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div> :
+                <div className="grid">
+                    <button className="navbar-toggler ml-auto" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon float-right " onClick={()=>{setOpen(true)}}></span>
+                    </button>
+                    <div className="mt-sm-20 ">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <a href="#home" className="nav-link">Главная</a>
+                            </li>
+
+                            <li className="nav-item">
+                                <a href="#resume" className="nav-link">Резюме</a>
+                            </li>
+                        </ul>
+                        <ul className="navbar-nav brand">
+                            <img src={avatar} alt="" className="brand-img"/>
+                            <li className="brand-txt">
+                                <h5 className="brand-title"></h5>
+                                <div className="brand-subtitle"> Developer</div>
+                            </li>
+                        </ul>
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <a href="#portfolio" className="nav-link">Портфолио</a>
+                            </li>
+
+                            <li className="nav-item last-item">
+                                <a href="#contact" className="nav-link">Контакты</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
+            }
+
         </nav>
     </div>
 }
