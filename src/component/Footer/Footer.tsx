caa47b9b2273f749ddeccaefb861ff65779ee0a2
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-07-10 20:31:40 +0300

    Update Footer.tsx

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer=()=> {
         <footer className="footer py-3">
             <div className="container">
                 <p className="small mb-0 text-light">
-<span>                   all rights reserved 2023</span>
+<span>               все права защищены 2023</span>
                 </p>
             </div>
         </footer>
